bd57ae08c45cd614fa6538571af34794c49e733c
Tom Brunkow <brunkotj@gmail.com>
2014-10-09 16:55:18 -0500

    First Commit

---
+++ Taskit/Date.swift
@@ -0,0 +1,9 @@
+//
+//  Date.swift
+//  Taskit
+//
+//  Created by Thomas on 10/7/14.
+//  Copyright (c) 2014 Tom Brunkow. All rights reserved.
+//
+
+import Foundation
---
+++ Taskit/TaskModel.swift
@@ -0,0 +1,9 @@
+//
+//  TaskModel.swift
+//  Taskit
+//
+//  Created by Thomas on 10/3/14.
+//  Copyright (c) 2014 Tom Brunkow. All rights reserved.
+//
+
+import Foundation
